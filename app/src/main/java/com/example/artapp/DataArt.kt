c3b2d241ab70eb76e7aabfe0f42989aa308371a3
Maximiliano Pelizzoni <112733459+ifMaxi@users.noreply.github.com>
2022-12-07 15:14:05 -0300

    Delete DataArt.kt
    Unused file

--- app/src/main/java/com/example/artapp/DataArt.kt
+++
@@ -1,9 +0,0 @@
-package com.example.artapp
-
-import androidx.annotation.StringRes
-import androidx.compose.ui.graphics.painter.Painter
-
-data class DataArt(
-    @StringRes val name: Int,
-    val painter: Painter
-)
